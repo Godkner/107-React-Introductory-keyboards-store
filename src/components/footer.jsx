3ba2fec819209a5ed489b28c3280514b48d9903c
Kevin Fierro <kfierro@uabc.edu.mx>
2023-01-26 19:36:49 -0800

    107ch-Class1

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+function Footer(){
+    return(
+        <div className='footer'>
+           <label >Kevin Fierro</label> 
+        </div>
+    );
+}
+
+export default Footer;
